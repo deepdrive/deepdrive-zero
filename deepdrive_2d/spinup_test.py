4c3390c4cd1c75bd6ac7170b3320867e750164ca
Craig Quiter <cquiter@gmail.com>
2019-12-10 14:17:17 -0800

    Eval plus accel

--- deepdrive_2d/spinup_test.py
+++ deepdrive_2d/spinup_test.py
@@ -5,7 +5,8 @@ import gym_match_input_continuous
 import deepdrive_2d
 import gym
 
-_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo/dd2d-ppo_s0')
-env = gym.make('deepdrive-2d-one-waypoint-v0')
+_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo-plus-accel/dd2d-ppo-plus-accel_s0')
+env = gym.make('deepdrive-2d-one-waypoint-plus-accel-v0')
 env.unwrapped.enable_render()
+# env.unwrapped.physics_steps_per_observation = 1
 run_policy(env, get_action)
