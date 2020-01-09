57896df783b9a769079595944c7dc9f59f6801f1
Craig Quiter <cquiter@gmail.com>
2020-01-08 18:13:55 -0800

    New test policy path

--- deepdrive_2d/spinup_test.py
+++ deepdrive_2d/spinup_test.py
@@ -5,7 +5,7 @@ import gym_match_input_continuous
 import deepdrive_2d
 import gym
 
-_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo-static-obstacle-snapshot/dd2d-ppo-static-obstacle_s0')
+_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo-static-obstacle-front-dist-no-gs/dd2d-ppo-static-obstacle-front-dist-no-gs_s0')
 env = gym.make('deepdrive-2d-static-obstacle-v0')
 env.unwrapped.enable_render()
 # env.unwrapped.physics_steps_per_observation = 1
