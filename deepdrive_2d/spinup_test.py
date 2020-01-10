68847b981de88611dea44c14dccdda251a7ecc9a
Craig Quiter <cquiter@gmail.com>
2020-01-10 10:45:09 -0800

    Test fine tune

--- deepdrive_2d/spinup_test.py
+++ deepdrive_2d/spinup_test.py
@@ -5,7 +5,7 @@ import gym_match_input_continuous
 import deepdrive_2d
 import gym
 
-_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo-static-obstacle-front-dist-no-gs/dd2d-ppo-static-obstacle-front-dist-no-gs_s0')
+_, get_action = load_policy('/home/c2/src/spinningup/data/dd2d-ppo-static-obstacle-fine-tune-gs/dd2d-ppo-static-obstacle-fine-tune-gs_s0', use_model_only=False)
 env = gym.make('deepdrive-2d-static-obstacle-v0')
 env.unwrapped.enable_render()
 # env.unwrapped.physics_steps_per_observation = 1
