6a38ae889399b0dc11504e2e50acfc53b32cac29
Craig Quiter <cquiter@gmail.com>
2020-01-27 17:36:59 -0800

    Add intersection envs

--- deepdrive_2d/envs/__init__.py
+++ deepdrive_2d/envs/__init__.py
@@ -1,4 +1,4 @@
 from deepdrive_2d.envs.env import Deepdrive2DEnv
 from deepdrive_2d.envs.one_waypoint_env import OneWaypointEnv, \
     OneWaypointPlusAccelEnv, IncentArrivalEnv, StaticObstacleEnv, \
-    NoGforcePenaltyEnv, SixtyFpsEnv
+    NoGforcePenaltyEnv, SixtyFpsEnv, IntersectionEnv
