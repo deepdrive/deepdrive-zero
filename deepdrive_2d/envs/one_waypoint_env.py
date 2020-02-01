caee5b2dda80ff0f785706a214e2f948d259358b
Craig Quiter <cquiter@gmail.com>
2020-02-01 14:30:45 -0800

    Okay, actually yes, this factorization may work for self-play

--- deepdrive_2d/envs/one_waypoint_env.py
+++ deepdrive_2d/envs/one_waypoint_env.py
@@ -3,35 +3,35 @@ from deepdrive_2d.envs import Deepdrive2DEnv
 
 class OneWaypointEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=True)
+        super().__init__(is_one_waypoint_map=True, match_angle_only=True)
 
 
 class OneWaypointPlusAccelEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=False)
+        super().__init__(is_one_waypoint_map=True, match_angle_only=False)
 
 
 class IncentArrivalEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=False,
+        super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True)
 
 
 class StaticObstacleEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=False,
+        super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True)
 
 class NoGforcePenaltyEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=False,
+        super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True,
                          disable_gforce_penalty=True)
 
 
 class SixtyFpsEnv(Deepdrive2DEnv):
     def __init__(self):
-        super().__init__(one_waypoint_map=True, match_angle_only=False,
+        super().__init__(is_one_waypoint_map=True, match_angle_only=False,
                          incent_win=True, add_static_obstacle=True,
                          disable_gforce_penalty=True,
                          physics_steps_per_observation=1)
