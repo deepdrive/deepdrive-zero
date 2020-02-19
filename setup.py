6d13a568d67e3223b5369fc28282e5cb32079c9f
shantanu <shantanu5996@gmail.com>
2020-02-18 23:16:03 -0800

    Add dependencies

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import setup
 
 setup(name='deepdrive_2d',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies we need
-)
+      # And any other dependencies we need
+      install_requires=['gym', 'numpy', 'scipy', 'arcade', 'loguru',
+                        'python-box', 'numba', 'matplotlib', 'gym',
+                        'retry', 'dataclasses']
+      )
