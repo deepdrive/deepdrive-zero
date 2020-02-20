2c1189e32b3baee9217ffc085b26f1ce50d61e9b
Craig Quiter <cquiter@gmail.com>
2020-02-20 14:02:40 -0800

    Inline collision penalty to make it easier to shape reward

--- deepdrive_2d/envs/agent.py
+++ deepdrive_2d/envs/agent.py
@@ -669,7 +669,12 @@ class Agent:
         self.angle_accuracies.append(angle_accuracy)
         info.stats.angle_accuracy = angle_accuracy
 
-        collision_penalty = self.get_collision_penalty(collided)
+        if collided:
+            # TODO: Make dependent on |Δv|
+            collision_penalty = pi
+        else:
+            collision_penalty = 0
+        collision_penalty *= 10
 
         win_reward = self.get_win_reward(won)
         ret = (
@@ -700,29 +705,6 @@ class Agent:
         # )
 
         return ret, info
-
-    @staticmethod
-    def get_collision_penalty(collided):
-        if collided:
-            # TODO: Increase this!
-            collision_penalty = pi
-        else:
-            collision_penalty = 0
-
-        collision_penalty *= 10
-
-        return collision_penalty
-
-    @staticmethod
-    def get_lane_penalty(left_lane_distance, right_lane_distance):
-        lane_penalty = 0
-        if left_lane_distance < 0:
-            lane_penalty += abs(left_lane_distance)
-        if right_lane_distance < 0:
-            # yes both can happen if you're orthogonal to the lane
-            lane_penalty += abs(right_lane_distance)
-        lane_penalty *= 2
-        return lane_penalty
 
     def get_win_reward(self, won):
         win_reward = 0
