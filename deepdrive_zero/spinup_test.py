e4b7c06ce83cd85b97f7c92b937c71c3015d3cea
Craig Quiter <cquiter@gmail.com>
2020-03-01 17:48:08 -0800

    Allow testing env configurable agents

--- deepdrive_zero/spinup_test.py
+++ deepdrive_zero/spinup_test.py
@@ -18,14 +18,25 @@ if TEST_STATIC_OBSTACLE:
 
     env = gym.make('deepdrive-2d-static-obstacle-no-g-pen-v0')
 else:
-    p = '/home/c2/src/spinningup/data/dd0-no-contraint-g-fine-tune-w-constraint-v0/dd0-no-contraint-g-fine-tune-w-constraint-v0_s0_2020_02-25_12-37.15/best_EpRet/2020_02-25_16-08.24'
+    p = '/home/c2/src/spinningup/data/dd0-ppo-one-waypoint-steer-only-delta-controls-no-g-pen-incent-win-fine-tune-jerk.01-g.1/dd0-ppo-one-waypoint-steer-only-delta-controls-no-g-pen-incent-win-fine-tune-jerk.01-g.1_s0_2020_03-01_11-39.33/best_EpRet/2020_03-01_15-57.19'
     if 'no-end-g' in p or 'no-contraint-g' in p or 'no-g' in p:
         os.environ['END_ON_HARMFUL_GS'] = '0'
+        os.environ['GFORCE_PENALTY_COEFF'] = '0'
+        os.environ['JERK_PENALTY_COEFF'] = '0'
     if 'no-contraint' in p and 'w-constraint' not in p:
         os.environ['CONSTRAIN_CONTROLS'] = '0'
+    if 'delta-controls' in p:
+        os.environ['EXPECT_NORMALIZED_ACTION_DELTAS'] = '1'
+    else:
+        os.environ['EXPECT_NORMALIZED_ACTION_DELTAS'] = '0'
+
+    if 'one-waypoint' in p:
+        env_name = 'deepdrive-2d-one-waypoint-v0'
+    else:
+        env_name = 'deepdrive-2d-intersection-v0'
     _, get_action = load_policy(p, use_model_only=False, deterministic=True)
     # env = gym.make('deepdrive-2d-intersection-v0')
-    env = gym.make('deepdrive-2d-intersection-w-gs-allow-decel-v0')
+    env = gym.make(env_name)
 
 # env.unwrapped.physics_steps_per_observation = 1
 run_policy(env, get_action)
