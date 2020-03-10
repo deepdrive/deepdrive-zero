52ffdd9563d90e03b571058691448be38eda9ae3
Craig Quiter <cquiter@gmail.com>
2020-03-10 12:48:35 -0700

    Adapt test model script for pytorch

--- deepdrive_zero/spinup_test.py
+++ deepdrive_zero/spinup_test.py
@@ -1,6 +1,7 @@
 import os
 
-from spinup.utils.test_policy import load_policy, run_policy
+from spinup.utils.test_policy import load_policy, run_policy, \
+    load_policy_and_env
 
 # Register custom envs
 import gym_match_input_continuous
@@ -19,14 +20,14 @@ if TEST_STATIC_OBSTACLE:
 
     env = gym.make('deepdrive-2d-static-obstacle-no-g-pen-v0')
 else:
-    p = '/home/c2/src/spinningup/data/dd0-ppo-one-waypoint-steer-only-delta-controls-no-g-pen-incent-win-cmn-inputs3/dd0-ppo-one-waypoint-steer-only-delta-controls-no-g-pen-incent-win-cmn-inputs3_s0_2020_03-03_13-30.40/best_HorizonReturn/2020_03-03_16-28.34'
-    if 'no-end-g' in p or 'no-contraint-g' in p or 'no-g' in p:
+    p = '/home/c2/src/tmp/spinningup/data/deepdrive-2d-intersection-no-constrained-controls/deepdrive-2d-intersection-no-constrained-controls_s0_2020_03-09_12-38.12/best_HorizonReturn/2020_03-10_11-31.22'
+    if 'no-end-g' in p or 'no-contraint-g' in p or 'no-g' in p or 'no-constrain' in p:
         os.environ['END_ON_HARMFUL_GS'] = '0'
         os.environ['GFORCE_PENALTY_COEFF'] = '0'
         os.environ['JERK_PENALTY_COEFF'] = '0'
-    if 'no-contraint' in p and 'w-constraint' not in p:
+    if 'no-constrain' in p:
         os.environ['CONSTRAIN_CONTROLS'] = '0'
-    if 'delta-controls' in p:
+    if 'delta-controls' in p or 'deepdrive-2d-intersection-no-g-or-jerk2' in p:
         os.environ['EXPECT_NORMALIZED_ACTION_DELTAS'] = '1'
     else:
         os.environ['EXPECT_NORMALIZED_ACTION_DELTAS'] = '0'
@@ -34,8 +35,8 @@ else:
     if 'one-waypoint' in p:
         env_name = 'deepdrive-2d-one-waypoint-v0'
     else:
-        env_name = 'deepdrive-2d-intersection-v0'
-    _, get_action = load_policy(p, use_model_only=False, deterministic=True)
+        env_name = 'deepdrive-2d-intersection-w-gs-allow-decel-v0'
+    _, get_action = load_policy_and_env(p, deterministic=True)
     # env = gym.make('deepdrive-2d-intersection-v0')
     env = gym.make(env_name)
 
