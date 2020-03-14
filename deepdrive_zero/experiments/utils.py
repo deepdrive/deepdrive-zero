c22c8b09db5978442f4b18d49f43335490b55960
Craig Quiter <cquiter@gmail.com>
2020-03-13 21:54:32 -0700

    Allow experiments to be tested

--- deepdrive_zero/experiments/utils.py
+++ deepdrive_zero/experiments/utils.py
@@ -6,7 +6,7 @@ from deepdrive_zero import player
 from spinup.utils.test_policy import load_policy_and_env, run_policy
 
 
-def run(train_fn, env_config):
+def run(train_fn, env_config, net_config=None):
     mode = '--train' if not sys.argv[1:] else sys.argv[1]
     if mode == '--train':
         train_fn()
@@ -14,10 +14,11 @@ def run(train_fn, env_config):
         player.start(env_config=env_config)
     elif mode == '--test':
         if not sys.argv[2:]:
-            model_path = get_latest_model_path()
+            model_path = get_latest_model_path()  # TODO
         else:
             model_path = sys.argv[2]
-        _, get_action = load_policy_and_env(model_path, deterministic=True)
         env = gym.make(env_config['env_name'])
         env.configure_env(env_config)
+        _, get_action = load_policy_and_env(fpath=model_path, deterministic=True,
+                                            env=env, net_config=net_config)
         run_policy(env, get_action)
